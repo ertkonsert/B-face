13c90aac36e897031ee8f82801cfa9d82dbd98a1
ertkonsert <martaaagren@gmail.com>
2021-02-17 10:04:42 +0100

    first commit

---
+++ b face/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace b_face
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
